453c5ebcfa5e31a3f21b6a634c25652e13bb95bd
Artem <supercrash2506@gmail.com>
2021-10-11 18:19:25 +0300

    Add new homework

---
+++ HomeWork2/HomeWork2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	cout << "Введите коефициенты a, b, c:" << endl;
+
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	int d = (b*b) - (4 * a*c);
+	int x1 = (-b + sqrt(d)) / (2 * a);
+	int x2 = (-b - sqrt(d)) / (2 * a);
+
+	cout << "x1 = " << x1 << endl;
+	cout << "x2 = " << x2 << endl;
+}
