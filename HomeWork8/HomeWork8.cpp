b35616f13379dfc211cdb06198e91a9d08503769
Artem <supercrash2506@gmail.com>
2021-12-03 12:11:06 +0200

    add new homework

---
+++ HomeWork8/HomeWork8.cpp
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+
+int main()
+{
+	string s;
+	getline(cin, s);
+
+	char char_array[200];
+	strcpy(char_array, s.c_str());
+
+	char* ch = strtok(char_array, " ,.-!?");
+	int n = 0;
+	string res = ch;
+
+	
+	while (ch != NULL)
+	{
+		string str1 = ch;
+
+		while (n < str1.length())
+		{
+			n++;
+		}
+
+		if (n > res.length())
+		{
+			res = ch;
+		}
+
+		n = 0;
+		ch = strtok(NULL, " ,.-!?");
+	}
+
+	cout << res;
+}
---
+++ HomeWork8_2/HomeWork8_2.cpp
@@ -0,0 +1,53 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+	string s;
+	getline(cin, s);
+
+	char char_array[200];
+	strcpy(char_array, s.c_str());
+
+	char* ch = strtok(char_array, " ,.-!?");
+	int n = 0;
+	int res = 0;
+
+	while (ch != NULL)
+	{
+		string str1 = ch;
+
+		while (n < str1.length())
+		{
+			n++;
+		}
+
+		for (int i = 1, j = 0; i <= str1.length(); i++)
+		{
+			if (str1[i] == str1[j])
+			{
+				n = 0;
+			}
+			for (int m = str1.length() - 1; m >= 0;m--)
+			{
+				if (str1[i] == str1[m] && i != m)
+				{
+					n = 0;
+				}
+			}
+			
+		}
+
+		if (n > res)
+		{
+			res = n;
+		}
+		ch = strtok(NULL, " ,.-!?");
+	}
+
+	cout << res;
+}
