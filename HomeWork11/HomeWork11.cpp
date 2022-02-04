6a97f78fa4a711950d654910fc97f5f1eba4dd51
Artem <supercrash2506@gmail.com>
2022-02-04 12:38:06 +0200

    add finished homework

---
+++ HomeWork11/HomeWork11.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	int n, res = 0, size;
+	cout << "Введите диапазон чисел: ";
+	cin >> n;
+
+	cout << "Введите кол-во чисел: ";
+	cin >> size;
+	cout << endl;
+
+	if (size > 500)
+	{
+		cout << "Вы ввели слишком большое кол-во чисел!" << endl;
+		return 0;
+	}
+
+	int arr[500];
+	srand(time(NULL));
+	int min = 0, max = n;
+
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % (max - min + 1) + min;
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << endl;
+	}
+
+	cout << endl;
+
+	for (int j = n; j >= 0; j--)
+	{
+		res = 0;
+
+		for (int i = 0; i < size; i++)
+		{
+			if (arr[i] == j)
+			{
+				res++;
+			}
+
+			if (i == size - 1 && res != 0)
+			{
+				cout << j << " Встретилось в " << res << (res == 1 ? " случае" : " случаях") << endl;
+			}
+		}
+	}
+}
