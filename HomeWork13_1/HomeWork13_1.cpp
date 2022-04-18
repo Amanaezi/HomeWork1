9b69eb75c3a6853b440fadf72d8e5e63663254be
Artem <91727914+Amanaezi@users.noreply.github.com>
2022-04-18 13:08:41 +0300

    add new finished homework

---
+++ HomeWork13_1/HomeWork13_1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, res = 0;
+    string str;
+	cin >> n >> str;
+
+	for (int i = 0; i < n; i++)
+	{
+		if (str[i] % 2 == 0)
+		{
+			res += (i + 1);
+		}
+	}
+
+	cout << res;
+}
---
+++ HomeWork13_2/HomeWork13_2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    int t = 0;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == 'a')
+        {
+            t++;
+        }
+    }
+
+    if (t > s.length() / 2)
+    {
+        cout << s.length();
+    }
+    else
+    {
+        cout << 2 * t - 1;
+    }
+}
